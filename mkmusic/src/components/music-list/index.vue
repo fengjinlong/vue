1eb625efff9a741b1755c429d705ac24b8ff6fb3
css <18618166564@163.com>
2017-11-02 22:02:32 +0800

    歌单列表
    歌单列表完成

--- mkmusic/src/components/music-list/index.vue
+++ mkmusic/src/components/music-list/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back" >
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div ref="playBtn" class="play" v-show="songs.length > 0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer">
@@ -14,12 +20,16 @@
       <div class="song-list-wrapper">
         <SongList :songs="songs"></SongList>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script>
 import Scroll from 'base/scroll'
+import loading from 'base/loading'
 import SongList from 'base/song-list'
 import {prefixStyle} from 'common/js/dom'
 const RESERVED_HEIGHT = 40
@@ -64,6 +74,9 @@ export default {
   methods: {
     scroll (pos) {
       this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
     }
   },
   watch: {
@@ -85,9 +98,11 @@ export default {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style[transform] = `scale(${scale})`
       this.$refs.bgImage.style.zIndex = zIndex
@@ -95,7 +110,8 @@ export default {
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    loading
   }
 }
 </script>
