979904d682b9e7b2ae4d49d03d9e67545b19e3f2
fengjinlong <18618166564.@163.com>
2017-12-05 17:29:57 +0800

    00
    00

--- mkmusic/src/common/js/cache.js
+++ mkmusic/src/common/js/cache.js
@@ -1,1 +1,12 @@
-export function saveSearch (query) {}
+import storage from 'good-storage'
+
+const SEARCH_KEY = '_search_'
+const SEARCH_MAX_LENGTH = 15
+
+function insertArray (arr, val, compare, maxLen) {
+  const index = arr.findIndex(compare)
+}
+
+export function saveSearch (query) {
+  let searches = storage.get(SEARCH_KEY, [])  
+}
