2cb6527a54caebd8937f2942982b859ad8b1fddf
fengjinlong <18618166564.@163.com>
2017-12-05 17:49:38 +0800

    1
    1

--- mkmusic/src/common/js/cache.js
+++ mkmusic/src/common/js/cache.js
@@ -5,8 +5,15 @@ const SEARCH_MAX_LENGTH = 15
 
 function insertArray (arr, val, compare, maxLen) {
   const index = arr.findIndex(compare)
+  if (index) {
+    return
+  }
+  if (index > 0) {
+    arr.splice(index, 1)
+  }
+  arr.unshift(val)
 }
 
 export function saveSearch (query) {
-  let searches = storage.get(SEARCH_KEY, [])  
+  let searches = storage.get(SEARCH_KEY, [])
 }
