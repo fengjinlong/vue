4418a52a3470fa464d6f2eb43bcde613d26967e5
fengjinlong <18618166564.@163.com>
2017-12-13 14:45:59 +0800

    1
    1

---
+++ musictwo/src/common/js/mixin.js
@@ -0,0 +1,130 @@
+import {mapGetters, mapMutations, mapActions} from 'vuex'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/util'
+
+export const playlistMixin = {
+  computed: {
+    ...mapGetters([
+      'playlist',
+      'sequenceList',
+      'currentSong',
+      'playlist',
+      'mode',
+      'favoriteList'
+    ])
+  },
+  mounted () {
+    this.handlePlaylist(this.playlist)
+  },
+  activated () {
+    this.handlePlaylist(this.playlist)
+  },
+  methods: {
+    // 如果组件没有定义handlePlaylist方法，就会执行mixin的hanglePlaylist方法
+    handlePlaylist () {
+      throw new Error('component must implement handlePlaylist method')
+    }
+  },
+  watch: {
+    playlist (newVal) {
+      this.handlePlaylist(newVal)
+    }
+  }
+}
+
+export const playerMixin = {
+  computed: {
+    iconMode () {
+      return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
+    },
+    ...mapGetters([
+      'sequenceList',
+      'currentSong',
+      'playlist',
+      'mode',
+      'favoriteList'
+    ])
+  },
+  methods: {
+    changeMode () {
+      const mode = (this.mode + 1) % 3
+      this.setPlayMode(mode)
+      let list = null
+      if (mode === playMode.random) {
+        list = shuffle(this.sequenceList)
+      } else {
+        list = this.sequenceList
+      }
+      this.resetCurrentIndex(list)
+      this.setPlayList(list)
+    },
+    resetCurrentIndex (list) {
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index)
+    },
+    getFavoriteIcon (song) {
+      if (this.isFavorite(song)) {
+        return 'icon-favorite'
+      }
+      return 'icon-not-favorite'
+    },
+    toggleFavorite (song) {
+      if (this.isFavorite(song)) {
+        this.deleteFavoriteList(song)
+      } else {
+        this.saveFavoriteList(song)
+      }
+    },
+    isFavorite (song) {
+      const index = this.favoriteList.findIndex((item) => {
+        return item.id === song.id
+      })
+      return index > -1
+    },
+    ...mapMutations({
+      setPlayState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayMode: 'SET_PLAY_MODE',
+      setPlayList: 'SET_PLAYLIST'
+    }),
+    ...mapActions([
+      'saveFavoriteList',
+      'deleteFavoriteList'
+    ])
+  }
+}
+
+export const searchMixin = {
+  data () {
+    return {
+      query: '',
+      refreshDelay: 100
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'searchHistory'
+    ])
+  },
+  methods: {
+    onQueryChange (query) {
+      // 处理带空格的情况
+      this.query = query.trim()
+    },
+    blurInput () {
+      this.$refs.searchBox.blur()
+    },
+    addquery (query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    saveSearch () {
+      this.saveSearchHistory(this.query)
+    },
+    ...mapActions([
+      'saveSearchHistory',
+      'deleteSearchHistory'
+    ])
+  }
+}
