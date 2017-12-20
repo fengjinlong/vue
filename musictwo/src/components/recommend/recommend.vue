9622d533b44f8784c675f6b288c9089e62d01a72
fengjinlong <18618166564.@163.com>
2017-12-20 16:12:47 +0800

    1
    1

--- musictwo/src/components/recommend/recommend.vue
+++ musictwo/src/components/recommend/recommend.vue
@@ -16,6 +16,15 @@
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
           <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
@@ -30,7 +39,8 @@
   export default {
     data () {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created () {
@@ -47,7 +57,9 @@
       },
       _getDiscList () {
         getDiscList().then((res) => {
-          console.log(res)
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
         })
       }
     },
