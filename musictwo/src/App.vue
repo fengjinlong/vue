86f5a6592767c785a5b400363ed3fbc1a21bb58d
fengjinlong <18618166564.@163.com>
2017-12-20 10:35:04 +0800

    1
    1

--- musictwo/src/App.vue
+++ musictwo/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <Mheader></Mheader>
     <Tab></Tab>
-    <router-view></router-view>
+    <keep-alive>
+      <router-view></router-view>      
+    </keep-alive> 
   </div>
 </template>
 
--- musictwo/src/base/slider/slider.vue
+++ musictwo/src/base/slider/slider.vue
@@ -41,10 +41,27 @@
         this._setSliderWidth()
         this._initDots()
         this._initSlider()
+
+        if (this.autoPlay) {
+          this._play()
+        }
       }, 20)
+      window.addEventListener('resize', () => {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()
+      })
     },
     methods: {
-      _setSliderWidth () {
+      _play () {
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          this.slider.next()
+        }, this.interval)
+      },
+      _setSliderWidth (isResize) {
         this.children = this.$refs.sliderGroup.children
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth
@@ -54,7 +71,7 @@
           child.style.width = sliderWidth + 'px'
           width += sliderWidth
         }
-        if (this.loop) {
+        if (this.loop && !isResize) {
           width += 2 * sliderWidth
         }
         this.$refs.sliderGroup.style.width = width + 'px'
@@ -64,11 +81,18 @@
           scrollX: true,
           scrollY: false,
           momentum: false,
-          click: true,
           snap: {
             loop: this.loop,
             threshold: 0.3,
             speed: 400
+          }
+        })
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          console.log(pageIndex)
+          this.currentPageIndex = pageIndex
+          if (this.autoPlay) {
+            this._play()
           }
         })
       },
